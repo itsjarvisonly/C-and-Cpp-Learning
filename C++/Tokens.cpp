ac0ae6f662885bbe13529e1decb1d266a27cd4c2
Ghanshyam Matlane <68685017+itsjarvisonly@users.noreply.github.com>
2025-09-30 11:33:20 +0000

    #

---
+++ C++/Tokens.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //same way of declaring as C
+    int a = 10;
+    double const b = 20;
+    char c = 'A';
+    float d = 5;
+    cout<<"The value a is "<<a<<"\n";
+    cout<<"The value of b is "<<b<<"\n";
+    cout<<"The value of c is "<<c<<"\n";
+    cout<<"The value of d is "<<d<<"\n";
+    return 0;
+/*similar program in C is named as 
+Tokens.c*/
+}
---
+++ C/Tokens.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    float b = 20;
+    double const c=30;
+    printf(" a is %d\n b is %.2f\n c is %lf\n",a,b,c);
+    return 0;
+    /*similar program in c++ is 
+    Tokens.cpp*/
+
+}
